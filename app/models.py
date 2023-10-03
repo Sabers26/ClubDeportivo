c2f8f577f0332c9dcd20840f34b2f3dab5e62980
LuisSaez <luissaez261002@gmail.com>
2023-10-03 17:31:24 -0300

    creacion de modelo de cuentas

--- app/models.py
+++ app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class CuentaUsuario(models.Model):
+    #Campos referentes a la cuenta
+    id_cuenta = models.UUIDField(primary_key=True, default=uuid.uuid4())
+    usuario = models.CharField(null=False, blank=False, max_length=50)
+    password = models.TextField(null=False, blank=False)
+
+    #Campos referentes al usuario
+    nombre_usuario = models.CharField(null=False, blank=False, max_length=60)
+
+    TIPO_SOCIO = [
+        ('Socio Colaborador', 'SocioColab'),
+        ('Socio', 'Socio')
+    ]
+
+    tipo_socio = models.TextField(null=False, blank=False, choices=TIPO_SOCIO, default=TIPO_SOCIO[1])
+
+class Cancha(models.Model):
+    pass
