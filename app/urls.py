e443382d9306066088a8478701919f076c224a3b
Giovanni Barra <gio.barra@duocuc.cl>
2023-10-18 13:50:16 -0300

    commit

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import inicio
+from .views import inicio, login_view, register
 
 urlpatterns = [
     path('', inicio, name='inicio'),
+    path('login', login_view, name='login'),
+    path('register', register, name='register'),
 ]
