ec9c7f75c6bad5ccba613af5cb3b7eb92db2d811
erwinnunez <er.nunez@duocuc.cl>
2023-10-24 15:06:24 -0300

    litio

--- app/views.py
+++ app/views.py
@@ -4,11 +4,9 @@ from django.shortcuts import render
 def inicio(request):
     return render(request, 'index.html')
 
-<<<<<<< HEAD
-=======
 def login_view(request):
     return render(request, 'login.html')
 
 def register(request):
     return render(request, 'register.html')
->>>>>>> rama_vistas
+
